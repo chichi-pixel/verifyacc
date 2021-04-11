c0d0fdd9d1b993c8583c545ef0232a93d0c4b70d
Chris <65090952+chriszabo@users.noreply.github.com>
2021-04-11 14:16:23 +0200

    input fields: fixed navigation, removed arrows

--- verify.js
+++ verify.js
@@ -1,17 +1,20 @@
-const codes = documents.querySelectorAll('.code');
+const codes = document.querySelectorAll('.code');
 
 codes[0].focus();
 
-codes.forEach((code, idx)); {
+codes.forEach((code, idx) => { 
     code.addEventListener('keydown', (e) => {
           if(e.key >= 0 && e.key <=9) {
-              codes[idx].value = '' /*is this line 8 code true?*/
-              setTimeout(() => codes[idx+1].focus(),
-              20);
+              //TODO: what if idx + 1 > codes.length?
+              setTimeout(() => codes[idx + 1].focus(),
+              20); 
           } else if(e.key === 'Backspace') {
+              //TODO: what if idx - 1 < 0 ?
               setTimeout(() => codes[idx - 1].focus(),
               20);
+          } else {
+            setTimeout(() => codes[idx].value = '',
+            20);
           }
-    })
-
-}
+    });
+});
